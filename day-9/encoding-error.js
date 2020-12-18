1d9e6f913b3160a40d9cd86a83e0ad7b95cdea33
sirchras <cjlynch@hotmail.co.nz>
2020-12-18 17:23:42 +1300

    Add solution for day-9 part 1

--- day-9/encoding-error.js
+++ day-9/encoding-error.js
@@ -1,3 +1,22 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt", "utf-8")
-  .split("\n");
+  .split("\n").slice(0, -1).map(x => Number(x));
+
+function verifyXMAS(num, preamble) {
+  for(let i = 0; i < preamble.length; i++) {
+    for(let j = 0; j < preamble.length; j++) {
+      if(i == j) continue;
+
+      if(preamble[i] + preamble[j] == num) return true;
+    }
+  }
+  return false;
+}
+
+for(let i = 25; i < input.length; i++) {
+  let preamble = input.slice(i - 25, i);
+  if(!verifyXMAS(input[i], preamble)) {
+    console.log(input[i]);
+    break;
+  }
+}
