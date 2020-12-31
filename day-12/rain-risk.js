a0642e77efc61c75c05c3f69db83b9641fb9c5a0
sirchras <cjlynch@hotmail.co.nz>
2020-12-31 16:17:48 +1300

    Refactor solution for day-12 part 1

--- day-12/rain-risk.js
+++ day-12/rain-risk.js
@@ -1,68 +1,88 @@
 const fs = require("fs");
-// const input = fs.readFileSync("./test.txt", "utf-8")
-const input = fs.readFileSync("./input.txt", "utf-8")
+const input = fs.readFileSync("./test.txt", "utf-8")
+// const input = fs.readFileSync("./input.txt", "utf-8")
   .split("\n").slice(0, -1)
   .map(itm => itm = {
     act: itm.substring(0, 1),
     val: Number(itm.substring(1))
   });
-const DIR = {
-  N: 0,
-  E: 90,
-  S: 180,
-  W: 270
-};
 
-function turnShip(turn) {
-  let directions = Object.keys(DIR);
-  // can't handle L turns going counter-clockwise past N
-  // need to use modulo (from mdn docs):
-  // ((a % n ) + n ) % n
-  let newFacing = (((DIR[facing] + turn) % 360) + 360) % 360;
+function createNewShip() {
+  const CIR = 360;
+  const DIR = { N: 0, E: 90, S: 180, W: 270 };
+  let x = 0, y = 0;
+  let facing = "E";
 
-  return directions.find(k => DIR[k] == newFacing);
-}
-
-function moveShip(pos, heading, dist) {
-  let {x, y} = pos;
-
-  switch(heading) {
-    case "N": case "S":
-      heading == "N" ? y += dist : y -= dist;
-      break;
-    case "E": case "W":
-      heading == "E" ? x += dist : x -= dist;
-      break;
+  function normalizeAngle(deg) {
+    return ((deg % CIR) + CIR) % CIR;
   }
 
-  return {x, y};
-}
+  function getNewFacing(deg) {
+    let directions = Object.keys(DIR);
 
-let x = 0, y = 0;
-let facing = "E";
-
-for(let i = 0; i < input.length; i++) {
-  // console.log(input[i]);
-  let {act, val} = input[i];
-  let heading = facing;
-  switch(act) {
-    case "N": case "E": case "S": case "W":
-      heading = act;
-    case "F":
-      // move in heading, val units
-      ({x, y} = moveShip({x, y}, heading, val));
-      // console.log(i, `moving ${heading} ${val} units to (${x}, ${y})`);
-      break;
-    case "L": case "R":
-      // turn
-      val = (act == "L" ? -val : val);
-      // console.log(facing, DIR[facing], act, val);
-      let newFacing = turnShip(val);
-      // console.log(i, `facing ${facing} turning ${act} to face ${newFacing}-${DIR[newFacing]}`);
-      facing = newFacing;
-      break;
+    return directions.find(k => DIR[k] == deg);
   }
+
+  let ship = {
+    turn: function(deg, clockwise = true) {
+      // console.log("turn", deg, clockwise);
+      let turn = (clockwise ? deg : -deg);
+      let curAngle = DIR[this.facing()];
+      let newAngle = normalizeAngle(curAngle + turn);
+      facing = getNewFacing(newAngle);
+
+      return this.facing();
+    },
+
+    move: function(dist, heading = this.facing()) {
+      // console.log("move", dist, heading);
+      switch(heading) {
+        case "N": case "S":
+          heading == "N" ? y += dist : y -= dist;
+          break;
+        case "E": case "W":
+          heading == "E" ? x += dist : x -= dist;
+          break;
+      }
+
+      return this.position();
+    },
+
+    position: function() {
+      return {x, y};
+    },
+
+    facing: function() {
+      return facing;
+    }
+  };
+
+  return ship;
 }
 
-console.log(`x: ${x}, y: ${y}`);
+function embarkOnVoyage(nav, ship = createNewShip()) {
+  for(let i = 0; i < nav.length; i++) {
+    let {act, val} = nav[i];
+    let heading;
+
+    switch(act) {
+      case "L": case "R":
+        ship.turn(val, act == "R");
+        break;
+      case "N": case "E": case "S": case "W":
+        heading = act;
+      case "F":
+        ship.move(val, heading);
+        break;
+    }
+  }
+
+  return ship;
+}
+
+// PART 1.
+let ship = embarkOnVoyage(input);
+let {x, y} = ship.position();
+
+console.log(ship.position());
 console.log(Math.abs(x) + Math.abs(y));
